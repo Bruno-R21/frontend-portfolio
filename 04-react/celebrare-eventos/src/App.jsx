c71b133266b3e79c0762e0a62ed23fadcdc364b0
Bruno-R21 <bruno21.conta.estudante@gmail.com>
2026-08-09 22:15:18 -0300

    feat: adiciona projeto Celebrare Eventos com React e Vite.

---
+++ 04-react/celebrare-eventos/src/App.jsx
@@ -0,0 +1,26 @@
+// App.jsx
+import './App.css'
+
+function App() {
+  return (
+    <main className="convite">
+      <section className="capa">
+        <p className="idade">40 ANOS</p>
+
+        <p className="frase-principal">
+          DE HISTÓRIAS, SONHOS E GRATIDÃO.
+        </p>
+
+        <p className="proximos-capitulos">
+          E QUE VENHAM OS PRÓXIMOS CAPÍTULOS!
+        </p>
+
+        <h1>RACHEL</h1>
+
+        <p className="celebre">CELEBRE A VIDA!</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
